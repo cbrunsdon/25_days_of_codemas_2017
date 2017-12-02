c50756d0f5f0f25a04b272bccaa3fbd8b242c4b9
Clarke Brunsdon <clarke@stembolt.com>
2017-12-01 19:14:31 -0800

    A two parter!?! Madness.

--- 01.rb
+++ 01.rb
@@ -4,5 +4,12 @@ string = input + input[0]
 total = (0..input.length).sum do |i|
   string[i] == string[i+1] ? string[i].to_i : 0
 end
-puts "Answer: "
+puts "Answer 1st part: "
+puts total
+
+puts "Answer 2nd part: "
+total = (0..input.length).sum do |i|
+  half = (i + (input.length / 2)) % input.length
+  input[i] == input[half] ? input[i].to_i : 0
+end
 puts total
