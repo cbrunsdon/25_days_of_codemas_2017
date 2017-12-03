f4ac3ad1bf9cdb755b983dd29e71bfe9d273f982
Clarke Brunsdon <clarke@stembolt.com>
2017-12-02 23:35:35 -0800

    Add second half of the puzzle

--- 02.rb
+++ 02.rb
@@ -1,9 +1,21 @@
 require 'csv'
 require 'pry'
-checksums = CSV.open('02.txt', { :col_sep => "\t" }).map do |line|
+lines = CSV.open('02.txt', { :col_sep => "\t" }).map do |line|
   vals = line.map(&:to_i)
-  vals.max - vals.min
 end
 
+puts "Answer to first half:"
+puts lines.map { |line| line.max - line.min }.sum
 
-puts checksums.sum
+def even_divisors(line)
+  line.sort.reverse.each do |num|
+    (line - [num]).each do |test|
+      if num % test == 0
+        return num / test
+      end
+    end
+  end
+end
+
+puts "Answer to second half:"
+puts lines.map { |line| even_divisors(line) }.sum
