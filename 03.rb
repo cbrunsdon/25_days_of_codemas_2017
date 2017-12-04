9e964e936e19e0dd96391e884120d1ab11678ed1
Clarke Brunsdon <clarke@stembolt.com>
2017-12-04 06:25:18 -0800

    Second half of three
    Getting preeeeety sloppy here.

--- 03.rb
+++ 03.rb
@@ -31,3 +31,81 @@ center = (square - 1) / 2
 steps = (pair[0] - center).abs + (pair[1] - center).abs
 
 puts "Answer is: #{steps}"
+
+class Grid
+  attr_reader :rows, :size
+
+  def initialize(size, previous_rows)
+    @size = size
+    @rows = previous_rows
+
+    return if size == 1
+    @rows = [Array.new(size)] + previous_rows.map do |row|
+      row.unshift nil
+      row << nil
+      row = row.flatten
+    end
+    @rows << Array.new(Array.new(size))
+
+    fill!
+  end
+
+  def print!
+    rows.each do |row|
+      puts row.join(" ")
+    end
+  end
+
+  private
+
+  def adjacents(x, y)
+    [maybe_at(x - 1, y),
+     maybe_at(x + 1, y),
+     maybe_at(x, y + 1),
+     maybe_at(x, y - 1),
+     maybe_at(x - 1, y + 1),
+     maybe_at(x - 1, y - 1),
+     maybe_at(x + 1, y - 1),
+     maybe_at(x + 1, y + 1)
+    ]
+  end
+
+  def maybe_at(x,y)
+    return nil if x < 0 or x > size - 1
+    return nil if y < 0 or y > size - 1
+
+    rows[x][y]
+  end
+
+  def fill!
+    fill_order.each do |x, y|
+      @rows[x][y] = adjacents(x,y).compact.sum
+      if @rows[x][y] >= 312051
+        puts @rows[x][y]
+        raise "yea this is fine"
+      end
+    end
+  end
+
+  def fill_order
+    steps = size - 1
+    all_fill = (0..(steps - 1)).map do |step|
+      [(size - step - 2), size - 1]
+    end
+    all_fill = all_fill +  (0..(steps - 1)).map do |step|
+      [0, size - step - 2]
+    end
+    all_fill = all_fill +  (0..(steps - 1)).map do |step|
+      [step + 1, 0]
+    end
+    all_fill + (0..(steps - 1)).map do |step|
+      [size - 1, step + 1]
+    end
+  end
+end
+
+grid = Grid.new(1, [[1]])
+while true
+  grid = Grid.new(grid.size + 2, grid.rows)
+  puts grid.print!
+end
