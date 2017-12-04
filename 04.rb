73459111bfb457f6d4542e809bdaf158340d03a5
Clarke Brunsdon <clarke@stembolt.com>
2017-12-04 06:32:29 -0800

    Day 4 was easier!

--- 04.rb
+++ 04.rb
@@ -3,3 +3,9 @@ valid_lines = File.open('04.txt', 'r').select do |line|
 end
 
 puts "There are #{valid_lines.count} valid lines"
+
+valid_lines = File.open('04.txt', 'r').select do |line|
+  line = line.split(" ").map { |y| y.chars.sort }
+  line.count == line.uniq.count
+end
+puts "There are #{valid_lines.count} valid with anagrams"
