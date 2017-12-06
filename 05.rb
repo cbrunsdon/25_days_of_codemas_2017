5f8bcdaae82696773c933a3a35d0a02ae4921bb2
Clarke Brunsdon <clarke@stembolt.com>
2017-12-06 02:24:04 -0800

    05.rb second half, I'm getting lazier!

--- 05.rb
+++ 05.rb
@@ -10,3 +10,19 @@ while index < steps.count
   number_steps = number_steps + 1
 end
 puts "First answer: #{number_steps}"
+
+index = 0
+puts number_steps = 0
+steps = File.open('05.txt').map &:to_i
+while index < steps.count
+  puts index
+  new_index = index + steps[index]
+  if steps[index] < 3
+    steps[index] = steps[index] + 1
+  else
+    steps[index] = steps[index] - 1
+  end
+  index = new_index
+  number_steps = number_steps + 1
+end
+puts "Second answer: #{number_steps}"
